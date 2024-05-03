c713fd0543f4e903d8e805b85b386bed550f4467
Davit Daraselia <d.daraselia@gmail.com>
2024-05-04 03:51:12 +0400

    image field

--- packages/data-provider/src/data-provider.ts
+++ packages/data-provider/src/data-provider.ts
@@ -4,6 +4,7 @@ import { getDocument } from "./get-document";
 import { getDocuments } from "./get-documents";
 import { getSuggestions } from "./get-suggestions";
 import { updateDocument } from "./update-document";
+import { upload } from "./upload";
 // import { getCollection } from "./get-collection";
 // import { getCollections } from "./get-collections";
 // import { createCollection } from "./create-collection";
@@ -21,8 +22,6 @@ import { updateDocument } from "./update-document";
 // import { getProject } from "./get-project";
 // import { getProjects } from "./get-projects";
 
-// import { upload } from "./upload";
-
 // import { createView } from "./create-view";
 // import { deleteView } from "./delete-view";
 // import { getView } from "./get-view";
@@ -32,45 +31,45 @@ import { DataProvider } from "@melony/core/types";
 import { init } from "./init";
 
 export const dataProvider = ({ apiUrl }: { apiUrl: string }): DataProvider => {
-  return {
-    // signIn: () => signIn(apiUrl),
+	return {
+		// signIn: () => signIn(apiUrl),
 
-    // createMember: (data) => createMember(apiUrl, data),
-    // deleteMember: (id, params) => deleteMember(apiUrl, id, params),
-    // getMembers: (params) => getMembers(apiUrl, params),
+		// createMember: (data) => createMember(apiUrl, data),
+		// deleteMember: (id, params) => deleteMember(apiUrl, id, params),
+		// getMembers: (params) => getMembers(apiUrl, params),
 
-    // getProject: (id) => getProject(apiUrl, id),
-    // getProjects: () => getProjects(apiUrl),
+		// getProject: (id) => getProject(apiUrl, id),
+		// getProjects: () => getProjects(apiUrl),
 
-    // upload: (data, projectId) => upload(apiUrl, data, projectId),
+		// createView: (data) => createView(apiUrl, data),
+		// deleteView: (id, projectId) => deleteView(apiUrl, id, projectId),
+		// getView: (id, projectId, params) => getView(apiUrl, id, projectId, params),
+		// getViews: (params) => getViews(apiUrl, params),
+		// updateView: (id, data) => updateView(apiUrl, id, data),
 
-    // createView: (data) => createView(apiUrl, data),
-    // deleteView: (id, projectId) => deleteView(apiUrl, id, projectId),
-    // getView: (id, projectId, params) => getView(apiUrl, id, projectId, params),
-    // getViews: (params) => getViews(apiUrl, params),
-    // updateView: (id, data) => updateView(apiUrl, id, data),
+		// addField: (collectionSlug, data) => addField(apiUrl, collectionSlug, data),
+		// getCollection: (slug, projectId) => getCollection(apiUrl, slug, projectId),
+		// getCollections: (params) => getCollections(apiUrl, params),
+		// createCollection: (data) => createCollection(apiUrl, data),
+		// updateCollection: (id, data) => updateCollection(apiUrl, id, data),
+		// deleteCollection: (slug, projectId) =>
+		//   deleteCollection(apiUrl, slug, projectId),
 
-    // addField: (collectionSlug, data) => addField(apiUrl, collectionSlug, data),
-    // getCollection: (slug, projectId) => getCollection(apiUrl, slug, projectId),
-    // getCollections: (params) => getCollections(apiUrl, params),
-    // createCollection: (data) => createCollection(apiUrl, data),
-    // updateCollection: (id, data) => updateCollection(apiUrl, id, data),
-    // deleteCollection: (slug, projectId) =>
-    //   deleteCollection(apiUrl, slug, projectId),
+		init: () => init(apiUrl),
 
-    init: () => init(apiUrl),
+		getDocument: (projectId, collectionSlug, id, params) =>
+			getDocument(apiUrl, projectId, collectionSlug, id, params),
+		getDocuments: (projectId, collectionSlug, params) =>
+			getDocuments(apiUrl, projectId, collectionSlug, params),
+		createDocument: (collectionSlug, data) =>
+			createDocument(apiUrl, collectionSlug, data),
+		updateDocument: (collectionSlug, id, data) =>
+			updateDocument(apiUrl, collectionSlug, id, data),
+		deleteDocument: (collectionSlug, id, projectId) =>
+			deleteDocument(apiUrl, collectionSlug, id, projectId),
+		getSuggestions: (projectId, params) =>
+			getSuggestions(apiUrl, projectId, params),
 
-    getDocument: (projectId, collectionSlug, id, params) =>
-      getDocument(apiUrl, projectId, collectionSlug, id, params),
-    getDocuments: (projectId, collectionSlug, params) =>
-      getDocuments(apiUrl, projectId, collectionSlug, params),
-    createDocument: (collectionSlug, data) =>
-      createDocument(apiUrl, collectionSlug, data),
-    updateDocument: (collectionSlug, id, data) =>
-      updateDocument(apiUrl, collectionSlug, id, data),
-    deleteDocument: (collectionSlug, id, projectId) =>
-      deleteDocument(apiUrl, collectionSlug, id, projectId),
-    getSuggestions: (projectId, params) =>
-      getSuggestions(apiUrl, projectId, params),
-  };
+		upload: (data, projectId) => upload(apiUrl, data, projectId),
+	};
 };
--- packages/ui/src/components/navigation.tsx
+++ packages/ui/src/components/navigation.tsx
@@ -3,41 +3,38 @@
 import { cn } from "@melony/ui/lib";
 
 export function Navigation({
-  children,
+	children,
 }: {
-  children: JSX.Element | JSX.Element[];
+	children: JSX.Element | JSX.Element[];
 }) {
-  return <div className="flex flex-col gap-2">{children}</div>;
+	return <div className="flex flex-col gap-2">{children}</div>;
 }
 
 export function NavigationItem({
-  title,
-  active,
-  onClick,
-  icon,
+	title,
+	active,
+	onClick,
+	icon,
 }: {
-  title?: string;
-  active?: boolean;
-  onClick?: () => void;
-  icon?: JSX.Element;
+	title?: string;
+	active?: boolean;
+	onClick?: () => void;
+	icon?: JSX.Element;
 }) {
-  return (
-    <div
-      className={cn(
-        "relative cursor-pointer inline-flex overflow-hidden items-center whitespace-nowrap text-sm ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 rounded-md px-3 justify-start",
-        {
-          "text-accent-foreground bg-muted": active,
-        }
-      )}
-      onClick={() => {
-        onClick && onClick();
-      }}
-    >
-      {active && (
-        <div className="absolute w-[4px] h-full bg-primary left-0 rounded-l-md"></div>
-      )}
-      {icon && icon}
-      <span className="block truncate">{title}</span>
-    </div>
-  );
+	return (
+		<div
+			className={cn(
+				"relative cursor-pointer inline-flex overflow-hidden items-center whitespace-nowrap text-sm ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-8 rounded-md px-2 justify-start",
+				{
+					"text-accent-foreground bg-muted": active,
+				},
+			)}
+			onClick={() => {
+				onClick && onClick();
+			}}
+		>
+			{icon && icon}
+			<span className="block truncate">{title}</span>
+		</div>
+	);
 }
