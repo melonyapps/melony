bf0d8135d93955bd64c4710c0a4423922ee0475e
Davit Daraselia <d.daraselia@gmail.com>
2024-05-03 15:20:56 +0400

    color field

--- packages/core/src/fields/index.ts
+++ packages/core/src/fields/index.ts
@@ -3,3 +3,4 @@ export { document } from "./document";
 export { documents } from "./documents";
 export { currency } from "./currency";
 export { password } from "./password";
+export { color } from "./color";
--- packages/views/src/constants.tsx
+++ packages/views/src/constants.tsx
@@ -5,25 +5,31 @@ import { FormCombobox } from "./components/form/form-combobox";
 import { FieldCurrency } from "./components/fields/field-currency";
 import { FieldPassword } from "./components/fields/field-password";
 import { FormPassword } from "./components/form/form-password";
+import { FieldColor } from "./components/fields/field-color";
+import { FormColor } from "./components/form/form-color";
 
 export const FIELDS: Record<
-  any,
-  { default: (props: any) => JSX.Element; form: (props: any) => JSX.Element }
+	any,
+	{ default: (props: any) => JSX.Element; form: (props: any) => JSX.Element }
 > = {
-  TEXT: {
-    default: FieldText,
-    form: FormInput,
-  },
-  CURRENCY: {
-    default: FieldCurrency,
-    form: FormInput,
-  },
-  DOCUMENT: {
-    default: FieldDocument,
-    form: FormCombobox,
-  },
-  PASSWORD: {
-    default: FieldPassword,
-    form: FormPassword,
-  },
+	TEXT: {
+		default: FieldText,
+		form: FormInput,
+	},
+	CURRENCY: {
+		default: FieldCurrency,
+		form: FormInput,
+	},
+	DOCUMENT: {
+		default: FieldDocument,
+		form: FormCombobox,
+	},
+	PASSWORD: {
+		default: FieldPassword,
+		form: FormPassword,
+	},
+	COLOR: {
+		default: FieldColor,
+		form: FormColor,
+	},
 };
